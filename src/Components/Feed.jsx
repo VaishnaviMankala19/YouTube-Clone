f0c81860118125fa6ce7a23ab18ff59bf655805d
mankalavaishnavi <mankalavaishnavi2004@gmail.com>
2024-05-31 23:15:53 +0530

    updating feed

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -7,7 +7,7 @@ const Feed = () => {
     <div>
       <Stack sx = {{ flexDirection : { sx:"column" , md: "row"}  }}>
         <Box sx ={{ height : {sx:"auto" , md:"92vh"}  , borderRight: '1px solid #3d3d3d' , px : {sx: 0 , md : 2} }} >
-          <SideBar></SideBar>
+          <SideBar/>
           <Typography className="copyright" varient="body2" sx = {{ mt : 1.5 , color : '#fff' }}>
                 copyright 2024 VM
           </Typography>
