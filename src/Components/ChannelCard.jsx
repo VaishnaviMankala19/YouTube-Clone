13f2f752b3da4c2fa70c67d9e06e5560149d7632
mankalavaishnavi <mankalavaishnavi2004@gmail.com>
2024-06-01 20:54:58 +0530

    started channelcard implementation

--- src/Components/ChannelCard.jsx
+++ src/Components/ChannelCard.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = () => {
+
+const ChannelCard = ({channelDetail}) => {
   return (
-    <div>
-      ChannelCard
-    </div>
+    <Box sx = {{ boxShadow : 'none' , borderRadius : '20px' }}  >
+
+    </Box>
   )
 }
 
