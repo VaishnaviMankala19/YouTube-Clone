7184b756388dbdd23e2f3260c5685fa3c13f2e17
mankalavaishnavi <mankalavaishnavi2004@gmail.com>
2024-07-21 17:49:55 +0530

    search functionality updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
-const NavBar = () => (
+const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
@@ -13,4 +13,4 @@ const NavBar = () => (
   </Stack>
 );
 
-export default NavBar;
+export default Navbar;
